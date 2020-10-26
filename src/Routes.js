779a73b2a1598bd00be7f0bb7d19764b23f8cdcc
Alexandr Rudenko <alexandr.rudenko@protonmail.com>
2020-10-26 23:01:46 +0100

    Add initial setup. Add router and Home page style

---
+++ src/Routes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import Cart from "./components/Cart";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/shop" component={Shop} />
+        <Route exact path="/cart" component={Cart} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="background"></div>
+      <div className="title-container">
+        <div className="title">
+          <h1>Take me to the shop</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
