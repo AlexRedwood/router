c56194bce704fff5d80474c0f7ec13e6aff86d28
Alexandr Rudenko <alexandr.rudenko@protonmail.com>
2020-11-30 22:26:22 +0100

    Add form to a contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,38 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="container contact">
-      <h1>Hello from Contact</h1>
+    <div className="flex-column container contact d-flex align-items-center justify-content-center">
+      <h1 className="mb-3 col-12 text-center">Contact Us</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="mail-contact"></label>
+          <input
+            type="email"
+            className="form-control"
+            id="mail-contact"
+            placeholder="Enter email..."
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="text-contact"></label>
+          <textarea
+            className="form-control"
+            id="text-contact"
+            placeholder="Leave the message..."
+            rows="6"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          id="submit-contact"
+          className="btn btn-outline-dark btn-block"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
